9ceada3821b14a70120f85cd654c6ee6b5d58532
Tianyu-Shen <77700209+Tianyu-Shen@users.noreply.github.com>
2021-10-18 18:39:06 -0500

    Add files via upload

--- ethereum_query.py
+++ ethereum_query.py
@@ -14,28 +14,56 @@ w3 = Web3(Web3.HTTPProvider('http://' + IP_ADDR + ':' + PORT))
 
 def get_transaction(tx):
     tx = {}   #YOUR CODE HERE
-    return tx
+    return w3.eth.get_transaction(tx)
 
 # Return the gas price used by a particular transaction,
 #   tx is the transaction
 def get_gas_price(tx):
     
-    return tx['gasPrice']
+    t=get_transaction(tx)
+
+    return t.get("gasPrice")  
 
 def get_gas(tx):
     
-    return tx['gas']
+    rec=w3.eth.get_transaction_receipt(tx)
+
+    return rec.get("gasUsed")
 
 def get_transaction_cost(tx):
     
-    return get_gas_price(tx)*get_gas(tx)
+    price=get_gas_price(tx)
+    a=get_gas(tx)
+    return a*price
 
 def get_block_cost(block_num):
     block_cost = 1  #YOUR CODE HERE
-    return block_cost
+    b=w3.eth.get_block(block_num)
+    ts=b1.get("transactions")
+
+    sum=0
+
+    for i in ts:
+        sum+=get_transaction_cost(i)
+
+    return sum
 
 # Return the hash of the most expensive transaction
 def get_most_expensive_transaction(block_num):
     max_tx = HexBytes('0xf7f4905225c0fde293e2fd3476e97a9c878649dd96eb02c86b86be5b92d826b6')  #YOUR CODE HERE
+    b=w3.eth.get_block(block_num)
+
+    ts=b.get("transactions")
+
+    high=0
+
+    for i in ts:
+        fee=get_transaction_cost(i)
+
+        if fee>high:
+            high=fee
+            tx=i
+
+    max_tx=HexBytes(tx)
     return max_tx
 
